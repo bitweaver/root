63e1ac498c6c6be84e8800b9b081e4ffa1fc8558
Michael Jennings <mej@users.sourceforge.net>
2005-11-21 23:41:14 +0000

    Mon Nov 21 18:41:12 2005                        Michael Jennings (mej)
    Changes needed to install bitweaver under PHP's safe_mode.
    ----------------------------------------------------------------------

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* @version $Header: /cvsroot/bitweaver/_root/index.php,v 1.2.2.4 2005/09/02 18:12:45 spiderr Exp $
+* @version $Header: /cvsroot/bitweaver/_root/index.php,v 1.2.2.5 2005/11/21 23:41:14 mej Exp $
 
 * @package bitweaver
 */
@@ -11,15 +11,15 @@
 
 // this has to be checked first thing since safe_mode screws up everything
 // to run bitweaver with safe_mode on, you should remove the following lines
-if( ini_get( 'safe_mode' ) ) {
-	echo '
-		<h1>Safe Mode check</h1>
-		We have detected that your server has <b>safe_mode</b> set to <b>on</b>.<br />
-		If you have access to the php.ini file or your service provider is willing, we recommend that you change this setting to <b>off</b><br />
-		To get bitweaver working with safe_mode active please click <a href="peartest.php">here</a>.<br />
-	';
-	die;
-}
+//if( ini_get( 'safe_mode' ) ) {
+//	echo '
+//		<h1>Safe Mode check</h1>
+//		We have detected that your server has <b>safe_mode</b> set to <b>on</b>.<br />
+//		If you have access to the php.ini file or your service provider is willing, we recommend that you change this setting to <b>off</b><br />
+//		To get bitweaver working with safe_mode active please click <a href="peartest.php">here</a>.<br />
+//	';
+//	die;
+//}
 require_once ('bit_setup_inc.php');
 
 //vd( $_COOKIE );
