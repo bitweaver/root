8e7628083ea49284761de75cf0b53d554c482692
Wakeworks <wakeworks@users.sourceforge.net>
2006-09-29 19:33:35 +0000

    *** empty log message ***

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -5,6 +5,5 @@
 
 	//ini_set('session.save_path', 'C:\somewhere\I\can\write');
 	define( 'BIT_ROOT_PATH', empty($_SERVER['VHOST_DIR']) ? dirname( __FILE__ ).'/' : $_SERVER['VHOST_DIR'].'/');
-
 	require_once( BIT_ROOT_PATH.'kernel/setup_inc.php' );
 ?>
