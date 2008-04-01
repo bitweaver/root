3a865936692d6172e49e0a43665fabda8912a2cc
Lester Caine <lester@lsces.co.uk>
2008-04-01 11:23:39 +0000

    phpDocumentor stuff has been tidied up TOO much - need this to give the index.html level documentation. Correct fix is to add comments TO index.php, but then all the index.php entries at a lower level need phpdoc comments added!

---
+++ phpdoc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author   lsces <lester@lsces.co.uk>
+ * @version  $Revision: 1.1 $
+ * @package  bitweaver
+ */
+
+/**
+ * Dummy package to add bitweaver documentation to index of phpDocumentor
+ */
+
+?>
