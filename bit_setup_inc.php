4be54b4100941336fb4472770eea3dcd7c2efcf1
Max Kremmel <xing@synapse.plus.com>
2008-10-03 10:14:15 +0000

    add code to allow for a version check and force a visit to the installer when a given version hasn't been reached yet.

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,9 +1,11 @@
 <?php
-/**
-* @package bitweaver
-*/
+//ini_set( 'session.save_path', 'C:\somewhere\I\can\write' );
+define( 'BIT_ROOT_PATH', empty( $_SERVER['VHOST_DIR'] ) ? dirname( __FILE__ ).'/' : $_SERVER['VHOST_DIR'].'/' );
+require_once( BIT_ROOT_PATH.'kernel/setup_inc.php' );
 
-	//ini_set('session.save_path', 'C:\somewhere\I\can\write');
-	define( 'BIT_ROOT_PATH', empty($_SERVER['VHOST_DIR']) ? dirname( __FILE__ ).'/' : $_SERVER['VHOST_DIR'].'/');
-	require_once( BIT_ROOT_PATH.'kernel/setup_inc.php' );
+// first thing we do, is check to see if our version of bitweaver is up to date
+if( !defined( 'BIT_INSTALL' ) && version_compare( '', $gBitSystem->getVersion(), '>' )) {
+	$gBitSmarty->display( "bitpackage:kernel/force_installer.tpl" );
+	die;
+}
 ?>
