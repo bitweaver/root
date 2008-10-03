8c57f2d3219546f5f0b119b4c90aca778cf17372
Max Kremmel <xing@synapse.plus.com>
2008-10-03 14:50:18 +0000

    add hack to allow users to log in when the installer force is called

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -4,8 +4,9 @@ define( 'BIT_ROOT_PATH', empty( $_SERVER['VHOST_DIR'] ) ? dirname( __FILE__ ).'/
 require_once( BIT_ROOT_PATH.'kernel/setup_inc.php' );
 
 // first thing we do, is check to see if our version of bitweaver is up to date
+// BIT_INSTALL is set by the installer and LOGIN_VALIDATE is set in users/validate.php
 // enter the version in the form '2.1.0-beta'
-if( !defined( 'BIT_INSTALL' ) && version_compare( '', $gBitSystem->getVersion(), '>' )) {
+if( !( defined( 'BIT_INSTALL' ) || defined( 'LOGIN_VALIDATE' )) && version_compare( '', $gBitSystem->getVersion(), '>' )) {
 	$gBitSmarty->display( "bitpackage:kernel/force_installer.tpl" );
 	die;
 }
