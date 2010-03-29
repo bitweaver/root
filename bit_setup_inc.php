2e91b12f43055e8caa897e65a14fad5f4254b134
Daniel Sutcliffe <dansut@users.sourceforge.net>
2010-03-29 18:45:50 +0000

    Typo in message for root bit_setup_inc.php being deprecated

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -6,7 +6,7 @@
 $rootPath = empty( $_SERVER['VHOST_DIR'] ) ? dirname( __FILE__ ).'/' : $_SERVER['VHOST_DIR'].'/';
 require_once( $rootPath.'kernel/setup_inc.php' );
 
-$out = "Deprecated file require_once call:\n\tfile: bit_setup_inc.php\n\tuse \"require_once( '../kernel/bit_setup_inc.php' );\"\n\tBacktrace to require_once source:\n\t";
+$out = "Deprecated file require_once call:\n\tfile: bit_setup_inc.php\n\tuse \"require_once( '../kernel/setup_inc.php' );\"\n\tBacktrace to require_once source:\n\t";
 $out .= bt( 1, FALSE );
 if( !defined( 'IS_LIVE' ) || IS_LIVE == FALSE ) {
 	vd( $out );
